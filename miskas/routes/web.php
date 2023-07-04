2862a2d9f773c7e5e9188a6ccb11cc278cfede2f
Jurgita <jurgita011@gmail.com>
2023-07-04 19:23:11 +0300

    laravel

--- miskas/routes/web.php
+++ miskas/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AnimalController as A;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
