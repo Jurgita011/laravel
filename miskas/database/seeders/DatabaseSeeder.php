dd0442e11bdee47567a12cd6f4e8caff5d60f582
Jurgita <jurgita011@gmail.com>
2023-07-05 15:07:30 +0300

    table db

--- miskas/database/seeders/DatabaseSeeder.php
+++ miskas/database/seeders/DatabaseSeeder.php
@@ -1,9 +1,11 @@
 <?php
-
+//cia duomenu bazeje isiraso user prisijungimas
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'Bebras',
+            'email' => 'bebras@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Briedis',
+            'email' => 'briedis@gmail.com',
+            'password' => Hash::make('123'),
+        ]);  
     }
 }
